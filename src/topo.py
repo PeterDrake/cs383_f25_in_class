94b4f28ebbc4d09d2680ed65090aa5b7c4caa450
Peter Drake <drake@lclark.edu>
2025-11-21 11:51:35 -0800

    topo improved

--- src/topo.py
+++ src/topo.py
@@ -1,20 +1,16 @@
 from digraph import Digraph
 
-def dfs(graph, v, visited=set()):  # TODO The set is only made once!
-    result = []
-    if v not in visited:
-        for w in graph.neighbors(v):
-            result += dfs(graph, w, visited)  # TODO Can this be done in constant time?
-        result.append(v)
-        visited.add(v)
-    return result
-
 def topo(graph):
     result = []
-    for v in range(len(graph.adj)):
-        vertices = dfs(graph, v)
-        for w in vertices:
-            result.append(w)
+    visited = set()
+    def dfs(v):
+        if v not in visited:
+            for w in graph.neighbors(v):
+                dfs(w)
+            result.append(v)
+            visited.add(v)
+    for vertex in range(len(graph.adj)):
+        dfs(vertex)
     return result[::-1]
 
 g = Digraph(9)
@@ -29,3 +25,14 @@ g.add_edge(1, 7)
 g.add_edge(6, 7)
 
 print(topo(g))
+print(topo(g))
+
+# def f(x=None):
+#     if x is None:
+#         x = []
+#     x.append('!')
+#     return x
+#
+# print(f())
+# print(f())
+# print(f([1, 2, 3]))
